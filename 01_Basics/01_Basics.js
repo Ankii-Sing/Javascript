d73deb25bca0f93a36d0c9303e70845194bd198b
Ankii-Sing <100915008+Ankii-Sing@users.noreply.github.com>
2024-01-31 18:48:10 +0000

    basic datatypes

---
+++ 01_Basics/01_Basics.js
@@ -0,0 +1,15 @@
+"use script"
+
+const ank = 55;
+let ak = "Ankit is my name"
+var boolean = true
+let pass = 13467
+var  stng = "I will be a Good Coder"
+randomvariable  =                                // this statement value is tereted as null value.
+noVar = null;
+
+// console.log(randomvariable)       // this will give an error. undefinded value
+// console.table([ank , ak, boolean , pass, stng,noVar , randomvariable])
+console.log(typeof boolean)
+console.table([typeof noVar , typeof pass , typeof ak , typeof randomvariable])        // we can print in the form of table also.
+
